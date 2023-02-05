0186349d6a22d66d249c18dd4082716cb86ddd53
Carlos Moraes <113129034+Carlos-Moraes2@users.noreply.github.com>
2023-02-04 21:34:42 -0300

    criando-variáveis

---
+++ app.js
@@ -0,0 +1,17 @@
+
+
+
+//Interração
+const citySearchBar = document.getElementById('city-search-input')
+const citySearchButton = document.getElementById('city-search-button')
+
+//Exibição
+const currentDate = document.getElementById("current-date");
+const cityName = document.getElementById("city-name");
+const weatherIcon = document.getElementById("weather-icon");
+const weatherDescription = document.getElementById("weather-description");
+const currentTemperature = document.getElementById("current-temperature");
+const windSpeed = document.getElementById("wind-speed");
+const fellsLikeTemperature = document.getElementById("fells-like-temperature");
+const currentHumidity = document.getElementById("current-humidity");
+const sunsetTime = document.getElementById("sunset-time");
